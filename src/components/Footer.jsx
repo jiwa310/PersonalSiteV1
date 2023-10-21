39f922633993d5ed49cd3a33279f1e2d17db94ae
Jim Wang <jimwang@ucsb.edu>
2023-10-20 22:39:42 -0700

    FINAL DRAFT

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="font-league-spartan text-center py-6 mt-12 text-gray-400">
       <p>Designed and Built by Jim Wang</p>
       <p>
-        <a href="https://github.com/yourusername/yourrepository" target="_blank" rel="noreferrer" className="font-league-spartan hover:text-yellow-400 hover:underline text-gray-400">
+        <a href="https://github.com/jiwa310/PersonalSiteV1" target="_blank" rel="noreferrer" className="font-league-spartan hover:text-yellow-400 hover:underline text-gray-400">
           GitHub Repository Link
           <img src="github.svg" alt="GitHub" className="inline-block w-6 h-6 ml-2" />
         </a>
